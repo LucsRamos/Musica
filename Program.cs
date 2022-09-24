0eed84f04d2a85f2c1cb542004fa97de00b27f96
Lucas Ramos <lucasramos.lr260@gmail.com>
2022-09-24 13:17:20 -0300

    Adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Jauperi - Minha Preta");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Meu amor coisa boa é te querer");
+Console.WriteLine("Coração tem Janelas pro mar");
+Console.WriteLine("Vento quente, ô África");
+Console.WriteLine("Me esquentar,");
+Console.WriteLine("Coisa boa é te querer");
+Console.WriteLine("Coração tem Janelas pro mar");
+Console.WriteLine("Vento quente, ô África");
+Console.WriteLine("Me esquentar");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Minha preta!");
+Console.WriteLine();
+
+Console.WriteLine("Óóó minha preta");
+Console.WriteLine("Saiba que pra mim, em tudo, você é melhor");
+Console.WriteLine("Óóó minha preta");
+Console.WriteLine("Saiba que pra mim, em tudo, você é melhor");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Alegria é nossa casa");
+Console.WriteLine("Nosso amor não tem igual");
+Console.WriteLine("Minha festa é animada");
+Console.WriteLine("Se tem você não falta nada");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Viu preta?!");
+Console.WriteLine();
+
+Console.WriteLine("Óóó minha preta");
+Console.WriteLine("Saiba que pra mim, em tudo, você é melhor");
+Console.WriteLine("Óóó minha preta");
+Console.WriteLine("Saiba que pra mim, em tudo, você é melhor");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Preta, minha preta");
+Console.WriteLine("Saiba que pra mim, em tudo, você é melhor");
+Console.WriteLine("Preta, minha preta");
+Console.WriteLine("Saiba que pra mim, em tudo, você é melhor");
